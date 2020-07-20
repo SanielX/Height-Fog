696e624d56ec795ed9da4581ff46bae4a837e8fd
SanielX <one.main.two@gmail.com>
2020-07-20 23:24:00 +0300

    Improvements

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 ![](./ForGit/Screenshot.png)
+![](./ForGit/Screenshot2.png)
 # Height-Fog
  Height fog implementation for Unity. Work in progress
  # How to use
 Like any Post Processing V2 shader. Add it as effect to Post Process volume.
 Though it does not support transparent geometry (And won't do it by default)
-
-### Todo:
-* Custom shader support
-* 3D Noise
-* Color from directional light (Any light?)
 
 Thanks [this](https://github.com/keijiro/DepthInverseProjection/blob/master/Assets/InverseProjection/Resources/InverseProjection.shader) repository for inverse projection code Xd
--- Resources/HeightFog.cs
+++ Resources/HeightFog.cs
@@ -9,10 +9,22 @@ using UnityEngine.Rendering.PostProcessing;
 public class HeightFogSettings : PostProcessEffectSettings
 {
     public ColorParameter FogColor = new ColorParameter { value = Color.white };
+    [UnityEngine.Rendering.PostProcessing.Min(0)]
     public FloatParameter FogDensity = new FloatParameter { value = 0.1f };
+
+    [UnityEngine.Rendering.PostProcessing.Min(0)]
     public FloatParameter MinFogHeight = new FloatParameter { value = 0f };
+
+    [UnityEngine.Rendering.PostProcessing.Min(0)]
     public FloatParameter MaxFogHeight = new FloatParameter { value = 5f };
+
     public FloatParameter DistanceDensity = new FloatParameter { value = 0.1f };
+    public Vector3Parameter WindDirection = new Vector3Parameter { value = Vector3.zero };
+
+    [Range(0, 1)]
+    public FloatParameter WindInfluence = new FloatParameter { value = 1 };
+
+    public BoolParameter LightColoring = new BoolParameter();
 }
 
 public class HeightFogRenderer : PostProcessEffectRenderer<HeightFogSettings>
@@ -27,6 +39,21 @@ public class HeightFogRenderer : PostProcessEffectRenderer<HeightFogSettings>
         sheet.properties.SetFloat("_DistanceDensity", settings.DistanceDensity);
         sheet.properties.SetColor("_FogColor", settings.FogColor);
         sheet.properties.SetFloat("_FogDensity", settings.FogDensity);
+        sheet.properties.SetVector("_WindDirection", settings.WindDirection);
+        sheet.properties.SetFloat("_WindInfluence", settings.WindInfluence);
+
+        var sun = RenderSettings.sun;
+        if (!sun || !settings.LightColoring)
+        {
+            sheet.DisableKeyword("LIGHT_ATTEN");
+            context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+            return;
+        }
+
+        sheet.EnableKeyword("LIGHT_ATTEN");
+        sheet.properties.SetColor("_DirectionalLightColor", sun.color);
+        var sunTransform = sun.transform;
+        sheet.properties.SetVector("_DirectionalLightVector", sunTransform.forward);
 
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
     }
